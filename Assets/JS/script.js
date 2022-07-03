c13d9e0c2ffbf340ec8d2bee65c7c7bf2048ed4a
Randie <srandall1213@gmail.com>
2022-07-02 23:15:26 -0600

    added files and outline

---
+++ Assets/JS/script.js
@@ -0,0 +1,4 @@
+//add event listener to start btn to begin timer on first "click" and reset on second "click"
+//display alert for win or lose and stop timer
+//scores persist on refresh (local storage?)
+//press key = key event 
